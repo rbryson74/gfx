46cf69026ad9e50a0a6268f959ca646dda34ca0a
Ryan Rogerson <ryan.rogerson@microchip.com>
2019-08-14 12:40:01 -0700

    [GFX] Fixed a warning
    MH3-18095

--- middleware/legato/library/src/gfx/legato/memory/legato_variableheap.c
+++ middleware/legato/library/src/gfx/legato/memory/legato_variableheap.c
@@ -146,7 +146,6 @@ static leResult _validateList(leVariableHeapBlockHeader* blk)
 #endif
 
 #if LE_USE_DEBUG_ALLOCATOR == 1
-#if 0
 static uint32_t _countList(leVariableHeapBlockHeader* blk)
 {
     uint32_t count = 0;
@@ -160,7 +159,6 @@ static uint32_t _countList(leVariableHeapBlockHeader* blk)
 
     return count;
 }
-#endif
 #endif
 
 static leVariableHeapBlockHeader* _findSlot(leVariableHeapBlockHeader* list, leVariableHeapBlockHeader* blk)
