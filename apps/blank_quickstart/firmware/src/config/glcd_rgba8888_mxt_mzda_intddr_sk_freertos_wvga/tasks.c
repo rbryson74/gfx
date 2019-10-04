d77356fd985ba901a20a6a282310a3b2ba8a32de
Keith Jones <keith.jones@microchip.com>
2019-10-04 16:38:51 -0700

    regen v3.5.0.1 blank applications

--- apps/blank_quickstart/firmware/src/config/glcd_rgba8888_mxt_mzda_intddr_sk_freertos_wvga/tasks.c
+++ apps/blank_quickstart/firmware/src/config/glcd_rgba8888_mxt_mzda_intddr_sk_freertos_wvga/tasks.c
@@ -53,6 +53,7 @@
 #include "configuration.h"
 #include "definitions.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: RTOS "Tasks" Routine
@@ -112,7 +113,6 @@ void _APP_Tasks(  void *pvParameters  )
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-
 void SYS_Tasks ( void )
 {
     /* Maintain system services */
@@ -170,7 +170,6 @@ void SYS_Tasks ( void )
     vTaskStartScheduler(); /* This function never returns. */
 
 }
-
 
 /*******************************************************************************
  End of File
