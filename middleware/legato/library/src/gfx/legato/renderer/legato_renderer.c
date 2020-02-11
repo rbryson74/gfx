4aa16cf92fb162f61c10b9c4dd2adfb39416dfdd
Keith Jones <keith.jones@microchip.com>
2020-02-11 16:09:17 -0700

    MH3-29666 removed legato/renderer/legato_renderer_pic32mz.c           added CACHE_ALIGN instead

--- middleware/legato/library/src/gfx/legato/renderer/legato_renderer.c
+++ middleware/legato/library/src/gfx/legato/renderer/legato_renderer.c
@@ -7,12 +7,13 @@
 #include "gfx/legato/common/legato_utils.h"
 #include "gfx/legato/core/legato_state.h"
 #include "gfx/legato/datastructure/legato_rectarray.h"
+#include "device.h"
 
 leRenderState _rendererState;
 
 #define SCRACH_BUFFER_SZ (LE_SCRATCH_BUFFER_SIZE_KB * 1024)
 
-static uint8_t scratchBuffer[SCRACH_BUFFER_SZ];
+static uint8_t CACHE_ALIGN scratchBuffer[SCRACH_BUFFER_SZ];
 static uint32_t maxScratchPixels;
 
 static lePixelBuffer renderBuffer;
