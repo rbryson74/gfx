f2a6bc43514a5a7dd39ba6935e0fff9893931b7b
Ed Gallofin <ed.gallofin@microchip.com>
2020-02-13 17:02:44 -0700

    MH3-29666 - Add gfx_driver interface to legato external controller driver generator

--- middleware/legato/driver/controller/external_controller/config/files.py
+++ middleware/legato/driver/controller/external_controller/config/files.py
@@ -1,6 +1,22 @@
 # generate code files
 projectPath = "config/" + Variables.get("__CONFIGURATION_NAME") + "/gfx/driver/external_controller"
 
+GFX_DRIVER_H = comp.createFileSymbol("GFX_DRIVER_H", None)
+GFX_DRIVER_H.setSourcePath("../../templates/gfx_driver.h.ftl")
+GFX_DRIVER_H.setDestPath("gfx/driver/")
+GFX_DRIVER_H.setOutputName("gfx_driver.h")
+GFX_DRIVER_H.setProjectPath(projectPath)
+GFX_DRIVER_H.setType("HEADER")
+GFX_DRIVER_H.setMarkup(True)
+
+GFX_DRIVER_C = comp.createFileSymbol("GFX_DRIVER_C", None)
+GFX_DRIVER_C.setSourcePath("../../templates/gfx_driver.c.ftl")
+GFX_DRIVER_C.setDestPath("gfx/driver/")
+GFX_DRIVER_C.setOutputName("gfx_driver.c")
+GFX_DRIVER_C.setProjectPath(projectPath)
+GFX_DRIVER_C.setType("SOURCE")
+GFX_DRIVER_C.setMarkup(True)
+
 GFX_EXTERNAL_CONTROLLER_C = comp.createFileSymbol("GFX_EXTERNAL_CONTROLLER_C", None)
 GFX_EXTERNAL_CONTROLLER_C.setDestPath("gfx/driver/controller/external_controller")
 GFX_EXTERNAL_CONTROLLER_C.setSourcePath("templates/drv_gfx_external_controller.c.ftl")
